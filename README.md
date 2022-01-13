d1354dd6bae5fa21755e2c9d2bc63e90e862310a
Bhavyai Gupta <zbhavyai@gmail.com>
2022-01-13 15:23:09 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Web Interface for `budlib-api`
+
+A web based front end for the [budlib-api](https://github.com/budlib/budlib-api)
+
+## How to run
+
+1. Clone the repository on your machine
+
+2. Run the below commands inside cloned directory
+
+    ```bash
+    $ npm install
+    $ npm start
+    ```
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <br />
+                <br />
+                <p>Coming up soon</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
