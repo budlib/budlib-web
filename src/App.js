25313b0dee798e5d090f1ae719630752eee629ab
Bhavyai Gupta <zbhavyai@gmail.com>
2022-02-05 02:22:51 -0700

    build: adding stuff common to all pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
+import React from 'react';
 import logo from './logo.jpg';
-import './App.css';
+import Sidebar from './components/sidebar';
+
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-    return (
-        <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <br />
-                <br />
-                <p>Coming up soon</p>
-            </header>
-        </div>
-    );
+  return (
+    // <div className='App'>
+    //     <header className='App-header'>
+    //         <img src={logo} className='App-logo' alt='logo' />
+    //         <br />
+    //         <br />
+    //         <p>Coming up soon</p>
+    //     </header>
+    // </div>
+
+    <Sidebar />
+
+    // <Router>
+    //     <Switch>
+    //         <Route path='/components/:type/:subtype/:name'>
+    //             <ComponentRenderer />
+    //         </Route>
+    //         <Route path='/components/:type/:name'>
+    //             <ComponentRenderer />
+    //         </Route>
+    //         <Route path='/thank-you'>
+    //             <ThankYouPage />
+    //         </Route>
+    //         <Route path='/'>
+    //             <MainLandingPage />
+    //         </Route>
+    //     </Switch>
+    // </Router>
+  );
 }
 
 export default App;
