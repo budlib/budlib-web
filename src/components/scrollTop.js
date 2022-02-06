95a39f4dab1f04f36f3c813aabad9c970152c98a
Bhavyai Gupta <zbhavyai@gmail.com>
2022-02-06 02:06:54 -0700

    build: adding scroll to top component

---
+++ src/components/scrollTop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const scrollTop = () => {
+  return (
+    <a className='scroll-to-top rounded' href='#page-top'>
+      <i className='fas fa-angle-up'></i>
+    </a>
+  );
+};
+
+export default scrollTop;
