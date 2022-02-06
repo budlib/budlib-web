66bcc7be50305e841d62ef27f1617beb296062d8
Bhavyai Gupta <zbhavyai@gmail.com>
2022-02-06 02:08:13 -0700

    build: adding draft of router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
 import React from 'react';
 import logo from './logo.jpg';
-import Sidebar from './components/sidebar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// pages
+import SearchBooks from './pages/SearchBooks';
 
 function App() {
   return (
-    // <div className='App'>
-    //     <header className='App-header'>
-    //         <img src={logo} className='App-logo' alt='logo' />
-    //         <br />
-    //         <br />
-    //         <p>Coming up soon</p>
-    //     </header>
-    // </div>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path='/components/:type/:subtype/:name'>
+          <ComponentRenderer />
+        </Route>
+        <Route path='/components/:type/:name'>
+          <ComponentRenderer />
+        </Route> */}
+        <Route path='/books/search' element={<SearchBooks />} />
 
-    <Sidebar />
-
-    // <Router>
-    //     <Switch>
-    //         <Route path='/components/:type/:subtype/:name'>
-    //             <ComponentRenderer />
-    //         </Route>
-    //         <Route path='/components/:type/:name'>
-    //             <ComponentRenderer />
-    //         </Route>
-    //         <Route path='/thank-you'>
-    //             <ThankYouPage />
-    //         </Route>
-    //         <Route path='/'>
-    //             <MainLandingPage />
-    //         </Route>
-    //     </Switch>
-    // </Router>
+        {/* <Route path='*'>
+          <ErrorPage />
+        </Route> */}
+        {/* <Route path='/thank-you'>
+          <ThankYouPage />
+        </Route>
+        <Route path='/'>
+          <MainLandingPage />
+        </Route> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
