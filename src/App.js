9b71bde67fe2d85d180d3834c3bdd485c718d8d4
Bhavyai Gupta <zbhavyai@gmail.com>
2022-02-13 03:38:39 -0700

    build: adding placeholders for what coming next, removing comments, refactor

--- src/App.js
+++ src/App.js
@@ -8,27 +8,26 @@ import AddBook from './pages/Books/AddBook';
 import SearchLoaners from './pages/Loaners/SearchLoaners';
 import ViewLoaner from './pages/Loaners/ViewLoaner';
 import AddLoaner from './pages/Loaners/AddLoaner';
-import Transactions from './pages/Transactions';
+import SearchTransactions from './pages/Transactions/SearchTransactions';
 import Error from './pages/Error';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path='/components/:type/:subtype/:name'>
-          <ComponentRenderer />
-        </Route>
-        <Route path='/components/:type/:name'>
-          <ComponentRenderer />
-        </Route> */}
         <Route path='/' element={<Navigate to='/home' />} />
         <Route path='/home' element={<Home />} />
         <Route path='/books/search' element={<SearchBooks />} />
+        {/* <Route path='/books/:id' element={<ViewBook />} /> */}
+        {/* <Route path='/books/:id/view' element={<ViewBook />} /> */}
         <Route path='/books/add-book' element={<AddBook />} />
         <Route path='/loaners/search' element={<SearchLoaners />} />
-        <Route path='/loaners/view/:id' element={<ViewLoaner />} />
+        <Route path='/loaners/:id' element={<ViewLoaner />} />
+        <Route path='/loaners/:id/view' element={<ViewLoaner />} />
+        {/* <Route path='/loaners/:id/edit' element={<EditLoaner />} /> */}
         <Route path='/loaners/add-loaner' element={<AddLoaner />} />
-        <Route path='/transactions' element={<Transactions />} />
+        <Route path='/transactions' element={<SearchTransactions />} />
+        {/* <Route path='/transactions/:id' element={<ViewTransaction />} /> */}
         <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
