864d0ecbcfbc3e85d027e4ceb57b31b09c6dea0a
Bhavyai Gupta <zbhavyai@gmail.com>
2022-02-14 02:36:13 -0700

    build: fixing routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 // pages
 import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 import SearchBooks from './pages/Books/SearchBooks';
 import AddBook from './pages/Books/AddBook';
 import SearchLoaners from './pages/Loaners/SearchLoaners';
@@ -18,16 +19,16 @@ function App() {
         <Route path='/' element={<Navigate to='/home' />} />
         <Route path='/home' element={<Home />} />
         <Route path='/books/search' element={<SearchBooks />} />
-        {/* <Route path='/books/:id' element={<ViewBook />} /> */}
         {/* <Route path='/books/:id/view' element={<ViewBook />} /> */}
+        {/* <Route path='/books/:id/edit' element={<ViewBook />} /> */}
         <Route path='/books/add-book' element={<AddBook />} />
         <Route path='/loaners/search' element={<SearchLoaners />} />
-        <Route path='/loaners/:id' element={<ViewLoaner />} />
         <Route path='/loaners/:id/view' element={<ViewLoaner />} />
         {/* <Route path='/loaners/:id/edit' element={<EditLoaner />} /> */}
         <Route path='/loaners/add-loaner' element={<AddLoaner />} />
         <Route path='/transactions' element={<SearchTransactions />} />
-        {/* <Route path='/transactions/:id' element={<ViewTransaction />} /> */}
+        {/* <Route path='/transactions/:id/view' element={<ViewTransaction />} /> */}
+        <Route path='/dashboard' element={<Dashboard />} />
         <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
