00e3873374ca09508b8cf48ad50488dbdfc30ae7
Mike L <firyace@gmail.com>
2022-02-15 20:15:18 -0700

    build: added search bar component

--- src/pages/Books/SearchBooks.js
+++ src/pages/Books/SearchBooks.js
@@ -5,8 +5,12 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import ScrollTop from '../../components/ScrollTop';
 import BookCards from '../../components/BookCards';
-
+import BookSearchBar from '../../components/BookSearchBar';
+import { useEffect, useState } from 'react';
 function SearchBooks() {
+  const [psearchBy, setSearchBy] = useState(["",""]);
+  const [psearchTerm, setSearchTerm] = useState("");
+  
   return (
     <React.Fragment>
       <div id='wrapper'>
@@ -16,7 +20,8 @@ function SearchBooks() {
           <div id='content'>
             <Header heading='Search books' />
             <div className='container-fluid'>
-              <BookCards />
+              <BookSearchBar func={setSearchBy}/>
+              <BookCards searchBy={psearchBy[0]} searchTerm = {psearchBy[1]}/>
             </div>
           </div>
 
