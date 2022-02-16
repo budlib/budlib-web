db2138f4a614b248b4ef13235e056ea1b2b7725a
Mike L <firyace@gmail.com>
2022-02-15 20:52:46 -0700

    build: added search function to loaner page

--- src/pages/Loaners/SearchLoaners.js
+++ src/pages/Loaners/SearchLoaners.js
@@ -5,8 +5,11 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import ScrollTop from '../../components/ScrollTop';
 import LoanerList from '../../components/LoanerList';
-
+import LoanerSearchBar from '../../components/LoanerSearchBar';
+import { useEffect, useState } from 'react';
 const SearchLoaners = () => {
+  const [psearchBy, setSearchBy] = useState(["",""]);
+  const [psearchTerm, setSearchTerm] = useState("");
   return (
     <React.Fragment>
       <div id='wrapper'>
@@ -16,7 +19,8 @@ const SearchLoaners = () => {
           <div id='content'>
             <Header heading='Search loaners' />
             <div className='container-fluid'>
-              <LoanerList />
+            <LoanerSearchBar func={setSearchBy}/>
+              <LoanerList searchBy={psearchBy[0]} searchTerm = {psearchBy[1]}/>
             </div>
           </div>
 
