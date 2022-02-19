04d6eae36c6a31cb3f2a55b27de5c179697936dc
Mike L <firyace@gmail.com>
2022-02-18 19:12:58 -0700

    build: login routings created

--- src/App.js
+++ src/App.js
@@ -18,28 +18,32 @@ import SelectBorrower from './pages/Loaners/SelectBorrower';
 import SelectReturner from './pages/Loaners/SelectReturner';
 import ReturnCart from './pages/Loaners/ReturnCart';
 import Error from './pages/Error';
+import Login from './pages/Login';
+
+import { RequireAuth } from './components/RequireAuth';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Navigate to='/home' />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/books/search' element={<SearchBooks />} />
-        <Route path='/books/:id/view' element={<ViewBook />} />
-        <Route path='/books/add-book' element={<AddBook />} />
-        <Route path='/books/:id/edit' element={<EditBook />} />
-        <Route path='/loaners/search' element={<SearchLoaners />} />
-        <Route path='/loaners/:id/view' element={<ViewLoaner />} />
-        <Route path='/loaners/add-loaner' element={<AddLoaner />} />
-        <Route path='/loaners/:id/edit' element={<EditLoaner />} />
-        <Route path='/transactions' element={<SearchTransactions />} />
+        <Route path='/' element={<RequireAuth><Navigate to='/home' /></RequireAuth>} />
+        <Route path='/home' element={<RequireAuth><Home /></RequireAuth>} />
+        <Route path='/books/search' element={<RequireAuth><SearchBooks /></RequireAuth>} />
+        <Route path='/books/:id/view' element={<RequireAuth><ViewBook /></RequireAuth>} />
+        <Route path='/books/add-book' element={<RequireAuth><AddBook /></RequireAuth>} />
+        <Route path='/books/:id/edit' element={<RequireAuth><EditBook /></RequireAuth>} />
+        <Route path='/loaners/search' element={<RequireAuth><SearchLoaners /></RequireAuth>} />
+        <Route path='/loaners/:id/view' element={<RequireAuth><ViewLoaner /></RequireAuth>} />
+        <Route path='/loaners/add-loaner' element={<RequireAuth><AddLoaner /></RequireAuth>} />
+        <Route path='/loaners/:id/edit' element={<RequireAuth><EditLoaner /></RequireAuth>} />
+        <Route path='/transactions' element={<RequireAuth><SearchTransactions /></RequireAuth>} />
         {/* <Route path='/transactions/:id/view' element={<ViewTransaction />} /> */}
-        <Route path='/loaners/return-books/:id' element={<ReturnCart />} />
-        <Route path='/loaners/return-books' element={<SelectReturner />} />
-        <Route path='/loaners/borrow-books/:id' element={<BorrowCart />} />
-        <Route path='/loaners/borrow-books' element={<SelectBorrower />} />
-        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/loaners/return-books/:id' element={<RequireAuth><ReturnCart /></RequireAuth>} />
+        <Route path='/loaners/return-books' element={<RequireAuth><SelectReturner /></RequireAuth>} />
+        <Route path='/loaners/borrow-books/:id' element={<RequireAuth><BorrowCart /></RequireAuth>} />
+        <Route path='/loaners/borrow-books' element={<RequireAuth><SelectBorrower /></RequireAuth>} />
+        <Route path='/dashboard' element={<RequireAuth><Dashboard /></RequireAuth>} />
+        <Route path="/login" element={<Login />} />
         <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
