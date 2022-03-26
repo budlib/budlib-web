2e4cf08ea07baa22424c8077fc541df1acddc97b
Bhavyai Gupta <zbhavyai@gmail.com>
2022-03-25 23:39:31 -0600

    fix: removing unused code

--- src/components/BookSearchBar.js
+++ src/components/BookSearchBar.js
@@ -22,54 +22,54 @@ function BookSearchBar(props) {
   };
 
   return (
-    filterOption,
-    filterText,
-    (
-      <div className='row'>
-        <div className='col-lg-12'>
-          <div className='card shadow'>
-            <div className='card-body'>
-              <div className='row'>
-                <div className='col-lg-4'>
-                  <div className='input-group mb-3'>
-                    <div className='input-group-prepend'>
-                      <span className='input-group-text' htmlFor='inputGroupSelect01'>
-                        Search by
-                      </span>
-                    </div>
-                    <select className='form-control form-select' id='selectFilter' onChange={(e) => handleFilter(e)} value={filterOption}>
-                      <option value=''>No Filter</option>
-                      <option value='title'>Title</option>
-                      <option value='author'>Author</option>
-                      <option value='publisher'>Publisher</option>
-                      <option value='isbn'>ISBN</option>
-                      <option value='tags'>Tags</option>
-                      <option value='librarysection'>Library Section</option>
-                      <option value='language'>Language</option>
-                    </select>
+    // filterOption,
+    // filterText,
+    // (
+    <div className='row'>
+      <div className='col-lg-12'>
+        <div className='card shadow'>
+          <div className='card-body'>
+            <div className='row'>
+              <div className='col-lg-4'>
+                <div className='input-group mb-3'>
+                  <div className='input-group-prepend'>
+                    <span className='input-group-text' htmlFor='inputGroupSelect01'>
+                      Search by
+                    </span>
                   </div>
+                  <select className='form-control form-select' id='selectFilter' onChange={(e) => handleFilter(e)} value={filterOption}>
+                    <option value=''>No Filter</option>
+                    <option value='title'>Title</option>
+                    <option value='author'>Author</option>
+                    <option value='publisher'>Publisher</option>
+                    <option value='isbn'>ISBN</option>
+                    <option value='tags'>Tags</option>
+                    <option value='librarysection'>Library Section</option>
+                    <option value='language'>Language</option>
+                  </select>
                 </div>
+              </div>
 
-                <div className='col-lg-6'>
-                  <div className='input-group mb-3'>
-                    <div className='input-group-prepend'>
-                      <span className='input-group-text'>Search term</span>
-                    </div>
-                    <input type='text' id='inputFilter' onChange={(e) => handleFilterText(e)} value={filterText} className='form-control' placeholder='Your search term goes here' />
+              <div className='col-lg-6'>
+                <div className='input-group mb-3'>
+                  <div className='input-group-prepend'>
+                    <span className='input-group-text'>Search term</span>
                   </div>
+                  <input type='text' id='inputFilter' onChange={(e) => handleFilterText(e)} value={filterText} className='form-control' placeholder='Your search term goes here' />
                 </div>
+              </div>
 
-                <div className='col-lg-2'>
-                  <button type='button' className='btn btn-primary btn-block' onClick={(e) => handleSearch(e)}>
-                    Search
-                  </button>
-                </div>
+              <div className='col-lg-2'>
+                <button type='button' className='btn btn-primary btn-block' onClick={(e) => handleSearch(e)}>
+                  Search
+                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
+    </div>
+    // )
   );
 }
 export default BookSearchBar;
--- src/components/LoanerSearchBar.js
+++ src/components/LoanerSearchBar.js
@@ -22,51 +22,47 @@ function LoanerSearchBar(props) {
   };
 
   return (
-    filterOption,
-    filterText,
-    (
-      <div className='row'>
-        <div className='col-lg-10'>
-          <div className='card shadow mb-4'>
-            <div className='card-body'>
-              <div className='row'>
-                <div className='col-lg-4'>
-                  <div className='input-group mb-3'>
-                    <div className='input-group-prepend'>
-                      <span className='input-group-text' htmlFor='inputGroupSelect01'>
-                        Search by
-                      </span>
-                    </div>
-                    <select className='form-control form-select' id='selectFilter' onChange={(e) => handleFilter(e)} value={filterOption}>
-                      <option value=''>No filter</option>
-                      <option value='id'>ID</option>
-                      <option value='schoolId'>School ID</option>
-                      <option value='name'>Name</option>
-                      <option value='parentName'>Parent's Name</option>
-                    </select>
+    <div className='row'>
+      <div className='col-lg-10'>
+        <div className='card shadow mb-4'>
+          <div className='card-body'>
+            <div className='row'>
+              <div className='col-lg-4'>
+                <div className='input-group mb-3'>
+                  <div className='input-group-prepend'>
+                    <span className='input-group-text' htmlFor='inputGroupSelect01'>
+                      Search by
+                    </span>
                   </div>
+                  <select className='form-control form-select' id='selectFilter' onChange={(e) => handleFilter(e)} value={filterOption}>
+                    <option value=''>No filter</option>
+                    <option value='id'>ID</option>
+                    <option value='schoolId'>School ID</option>
+                    <option value='name'>Name</option>
+                    <option value='parentName'>Parent's Name</option>
+                  </select>
                 </div>
+              </div>
 
-                <div className='col-lg-6'>
-                  <div className='input-group mb-3'>
-                    <div className='input-group-prepend'>
-                      <span className='input-group-text'>Search term</span>
-                    </div>
-                    <input type='text' id='inputFilter' onChange={(e) => handleFilterText(e)} value={filterText} className='form-control' placeholder='Your search term goes here' />
+              <div className='col-lg-6'>
+                <div className='input-group mb-3'>
+                  <div className='input-group-prepend'>
+                    <span className='input-group-text'>Search term</span>
                   </div>
+                  <input type='text' id='inputFilter' onChange={(e) => handleFilterText(e)} value={filterText} className='form-control' placeholder='Your search term goes here' />
                 </div>
+              </div>
 
-                <div className='col-lg-2'>
-                  <button type='button' className='btn btn-primary btn-block' onClick={(e) => handleSearch(e)}>
-                    Search
-                  </button>
-                </div>
+              <div className='col-lg-2'>
+                <button type='button' className='btn btn-primary btn-block' onClick={(e) => handleSearch(e)}>
+                  Search
+                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
+    </div>
   );
 }
 export default LoanerSearchBar;
