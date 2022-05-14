702153126cbad1dbeb2c3277a5370897cda581c1
Bhavyai Gupta <zbhavyai@gmail.com>
2022-05-14 01:03:32 -0600

    fix: reverting import order

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './assets/datatables/dataTables.bootstrap4.min.css';
 import './assets/fontawesome-free/css/all.min.css';
-import './assets/sb-admin/custom.css';
 import './assets/sb-admin/theme.css';
+import './assets/sb-admin/custom.css';
 import './assets/sb-admin/theme.js';
+import './assets/datatables/dataTables.bootstrap4.min.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
